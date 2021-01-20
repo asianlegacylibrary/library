6d290920073161dbb99f525cf6acd431bb3a9721
joelcrawford <joelrootcrawford@gmail.com>
2021-01-20 10:59:36 -0800

    added URL param reset on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,28 @@
 import '../assets/css/Header.scss'
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
 import logo from '../assets/img/logo.png'
+
+import { actions } from '../store/types'
 
 export function Header() {
     //let location = useLocation()
+    const dispatch = useDispatch()
+
     return (
         <header className='header'>
             <div className='header-container'>
-                <Link to='/'>
+                <a
+                    href='/'
+                    onClick={() => dispatch({ type: actions.RESET_URL_PARAMS })}
+                >
                     <img
                         className='img-logo'
                         src={logo}
                         alt='logo'
                         width='50px'
                     />
-                </Link>
+                </a>
                 <p className='header-title'>
                     API{' '}
                     {/* <span className='show-params'>
